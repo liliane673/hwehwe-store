01c9977240e8c17d60467f781da2722717f88dfd
farhanhakimm <farhanhakimmm@gmail.com>
2024-05-23 14:03:14 +0700

    UpdateNewCart

--- checkout.js
+++ checkout.js
@@ -46,8 +46,7 @@ function renderTrolleyCard() {
                 <div>
                 <button
                     type="button"
-                    class="px-2 rounded-start text-white cursor-pointer text-lg hover"
-                    style="font-size: 18px; background-color: #1ec88a"
+                    class="px-2 rounded-circle text-white cursor-pointer button1"
                     onclick=""
                 >
                     -
@@ -55,8 +54,7 @@ function renderTrolleyCard() {
                 <span id="valueQty" class="px-1">${cartArray[i].quantity}</span>
                 <button
                     type="button"
-                    class="px-1 rounded-end text-white cursor-pointer"
-                    style="font-size: 18px; background-color: #1ec88a"
+                    class="px-1 rounded-circle text-white cursor-pointer button1"
                     onclick=""
                 >
                     +
