5f9be402e3cda394063be4b7030a248c8d6cb83a
liliane673 <lilianeernestin673@gmail.com>
2024-05-24 01:23:39 +0700

    Finished all :)

--- homeFront.js
+++ homeFront.js
@@ -1,24 +1,30 @@
 
-import { allElectronicsData } from "./list-database.js";
+// import { allElectronicsData } from "./list-database.js";
 
 
- export function renderType() {
-    let typeObject = {};
-    for (let i = 0; i < allElectronicsData.length; i++) {
-      let eachData = allElectronicsData[i];
-      // console.log(eachData.type);
+ export function renderType(allElectronicsData) {
+    console.log(allElectronicsData);
 
-      if (typeObject[eachData.type] === undefined) {
-        typeObject[eachData.type] = 0;
+    if(allElectronicsData!== undefined){
+      let typeObject = {};
+      for (let i = 0; i < allElectronicsData.length; i++) {
+        let eachData = allElectronicsData[i];
+        // console.log(eachData.type);
+  
+        if (typeObject[eachData.type] === undefined) {
+          typeObject[eachData.type] = 0;
+        }
+        typeObject[eachData.type]++;
       }
-      typeObject[eachData.type]++;
-    }
-    // console.log(typeObject)
+      // console.log(typeObject)
+  
+      let renderType = document.getElementById("render-type");
 
-    let renderType = document.getElementById("render-type");
+    }
   }
 
-export function renderCard() {
+export function renderCard(allElectronicsData) {
+  if(allElectronicsData!== undefined){
     let renderCard = document.getElementById("renderCard");
     // console.log(renderCard);
 
@@ -49,7 +55,9 @@ export function renderCard() {
                         </span>
                     </div>
                 </div>
-                <div class="keranjang-button cursor-pointer">
+                <div 
+                data-product-id="${allElectronicsData[i].id}"
+                class="keranjang-button cursor-pointer button-cart">
     
                     Tambah Keranjang
     
@@ -58,135 +66,17 @@ export function renderCard() {
             </div>
             `;
     }
+
   }
+    
+}
+
 
 //showCards Function
-function showCards(allElectronicsData) {
-  renderType();
-  renderCard();
-}
-showCards(allElectronicsData);
-
-//Ngasih liat kategori yang dipiilih
-let filteredElectronicsData = [];
-let iskategoriClicked = `false`;
-
-let kategori = document.getElementsByClassName("produk-kategori");
-let chosenKategori = 4;
-for (let i = 0; i < kategori.length; i++) {
-  kategori[i].addEventListener("click", () => {
-    for (let j = 0; j < kategori.length; j++) {
-      kategori[j].style = "";
-    }
-    if (iskategoriClicked == `false` || iskategoriClicked != i) {
-      kategori[i].style =
-        "color: #f27c05; font-weight:500; border-bottom: 4px solid #f27c05;";
-      iskategoriClicked = i;
-      chosenKategori = i;
-      console.log(chosenKategori);
-    } else if (iskategoriClicked == i) {
-      kategori[i].style = "";
-      iskategoriClicked = `false`;
-      chosenKategori = 4;
-    }
-
-    //filtering
-    let typeKategori = [`Handphone`, `Laptop`, `TV`, `SmartWatch`];
-    console.log(typeKategori[chosenKategori]);
-
-    filteredElectronicsData = [];
-    for (let i = 0; i < allElectronicsData.length; i++) {
-      if (typeKategori[chosenKategori] === allElectronicsData[i].type) {
-        filteredElectronicsData.push(allElectronicsData[i]);
-      }
-    }
-
-    if (typeKategori[chosenKategori] === undefined) {
-      showCards(allElectronicsData);
-    } else {
-      showCards(filteredElectronicsData);
-    }
-  });
+export function showCards(allElectronicsData) {
+  renderType(allElectronicsData);
+  renderCard(allElectronicsData);
 }
 
-//Sticky Header
-window.onscroll = function () {
-  myFunction();
-};
-var header = document.getElementsByClassName("header")[0];
-var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-// let keranjangButton = document.getElementsByClassName(`keranjang-button`)
-// for(let i = 0; i < keranjangButton.length; i++){
-//     keranjangButton[i].addEventListener("mousedown",() =>{
-//         keranjangButton[i].style = "background-color: #ffc56e"
-//     })
-//     keranjangButton[i].addEventListener("mouseup",() =>{
-//         keranjangButton[i].style = "background-color: #f27c05"
-//     })
-//     keranjangButton[i].addEventListener('mouseleave', () => {
-//         if (!keranjangButton[i].matches(':hover')) {
-//             keranjangButton[i].style.backgroundColor = '#f27c05';
-//         }
-//     });
-// }
-
-//SEARCH FUNCTION
-function getWordsFromSentence(title) {
-  let hasilWords = [];
-  let temp = "";
-  for (let i = 0; i <= title.length; i++) {
-    if (title[i] == " " || title[i] === undefined) {
-      hasilWords.push(temp);
-      temp = "";
-    } else temp += title[i];
-  }
-  return hasilWords;
-}
-
-function dekonstruksiData(allElectronicsData) {
-  let hasil = {};
-  for (let i = 0; i < allElectronicsData.length; i++) {
-    hasil[allElectronicsData[i].id] = [];
-    hasil[allElectronicsData[i].id].push(allElectronicsData[i].brand);
-    hasil[allElectronicsData[i].id].push(allElectronicsData[i].type);
-    let hasilPemisahanTitle = getWordsFromSentence(allElectronicsData[i].title);
-    for (let j = 0; j < hasilPemisahanTitle.length; j++) {
-      hasil[allElectronicsData[i].id].push(hasilPemisahanTitle[j]);
-    }
-  }
-  return hasil;
-}
-
-function hasilSearch(input) {
-  let data = dekonstruksiData(allElectronicsData);
-  let hasilFilterSearch = [];
-  for (let key in data) {
-    for (let i = 0; i < data[key].length; i++) {
-      if (input == data[key][i]) {
-        for (let j = 0; j < allElectronicsData.length; j++) {
-          if (allElectronicsData[j].id == key) {
-            hasilFilterSearch.push(allElectronicsData[j]);
-          }
-        }
-      }
-    }
-  }
-  return hasilFilterSearch;
-}
-
-let searchButton = document.getElementsByClassName("search-button")[0];
-searchButton.addEventListener("click", () => {
-  let input = document.getElementsByClassName("search-bar")[0].value;
-  let dataHasilSearch = hasilSearch(input);
-  showCards(dataHasilSearch);
-});
 
